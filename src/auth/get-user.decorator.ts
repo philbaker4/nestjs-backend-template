dd8b3c4bbd84ff9d597556ac760293391aee450d
Philip Baker <philip.baker21@gmail.com>
2019-12-20 00:01:32 -0500

    auth: roles based authorization added

---
+++ src/auth/get-user.decorator.ts
@@ -0,0 +1,5 @@
+import { ReflectMetadata, createParamDecorator } from '@nestjs/common';
+
+export const GetUser = createParamDecorator((data, req) => {
+    return req.user;
+});
--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,21 +1,25 @@
-import { Get, Post, Body, Controller, Param, ValidationPipe, UsePipes, ParseIntPipe } from '@nestjs/common';
+import { Get, Post, Body, Controller, Param, ValidationPipe, UsePipes, ParseIntPipe, UseGuards } from '@nestjs/common';
 import { ItemsService } from './items.service';
 import { CreateItemDto } from './dto/create-item';
 import { Item } from './entity/item.entity';
-
+import { GetUser } from 'src/auth/get-user.decorator';
+import { Roles } from 'src/auth/roles.decorator';
+import { RolesGuard } from 'src/auth/roles.guard';
 
 @Controller('items')
 export class ItemsController {
   constructor(private readonly itemsService: ItemsService) {}
 
   @Get()
-  async getAll(): Promise<Item[]> {
+  @Roles('mtmproGlobalAdmin')
+  @UseGuards(RolesGuard)
+  async getAll(@GetUser() user: any): Promise<Item[]> {
     return this.itemsService.getAllItems();
   }
-  
+
   @Get('/:id')
   findItem(@Param('id', ParseIntPipe) id: number): Promise<Item> {
-      return this.itemsService.find(id)
+      return this.itemsService.find(id);
   }
 
   @UsePipes(ValidationPipe)
