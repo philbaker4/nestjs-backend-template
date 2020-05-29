2f528f80f7cfdc779580c903ee6c2218e1dcc354
Baker, Philip <pbaker@indians.com>
2020-05-29 16:39:21 -0400

    Item: update service to use true repository pattern

--- src/items/repository/item.repository.ts
+++ src/items/repository/item.repository.ts
@@ -1,10 +1,25 @@
 import { EntityRepository, Repository } from "typeorm";
 import { Item } from "../entity/item.entity";
 import { CreateItemDto } from "../dto/create-item";
+import { NotFoundException } from "@nestjs/common";
 
 
 @EntityRepository(Item)
 export class ItemRepository extends Repository<Item> {
+
+    async getItems(): Promise<Item[]> {
+        const query = this.createQueryBuilder('item');
+        const items = await query.getMany();
+        return items;
+    }
+
+    async findItem(id: number): Promise<Item> {
+        const found = await this.findOne(id);
+        if (!found) {
+            throw new NotFoundException(`Item with id ${id} not found.`);
+        }
+        return found;
+    }
     async createItem(createItemDto: CreateItemDto): Promise<Item> {
         const item = new Item();
         item.name = createItemDto.name;
