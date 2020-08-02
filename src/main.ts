4460283823b66c3de2b9373960771889a7f3e27e
Philip Baker <philip.baker21@gmail.com>
2020-08-02 13:19:50 -0400

    add helmet, gcp app engine deployment health check routes

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
 import 'module-alias/register';
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+// import * as helmet from 'helmet'
+const helmet = require('helmet')
+
+require('dotenv').config()
+import * as Express from 'express';
+import * as cors from 'cors';
+
 import { AppModule } from './app.module';
 
+const server = Express();
+server.use(helmet());
+server.use(cors());
+server.get('/', (req, res) => res.send('ok'));
+server.get('/_ah/health', (req, res) => res.send('ok'));
+server.get('/_ah/start', (req, res) => res.send('ok'));
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  // app.setGlobalPrefix('api');
+  await app.listen(process.env.PORT);
 }
 bootstrap();
